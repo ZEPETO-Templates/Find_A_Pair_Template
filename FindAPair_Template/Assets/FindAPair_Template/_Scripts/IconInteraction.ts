5a6bec78b4359a1c538dd8dc498377655091edfe
ilian ostapovich <dl_zptworldtemplates@naverz-corp.com>
2023-08-09 16:07:00 -0300

    V.0.0.52

--- FindAPair_Template/Assets/FindAPair_Template/_Scripts/IconInteraction.ts
+++ FindAPair_Template/Assets/FindAPair_Template/_Scripts/IconInteraction.ts
@@ -3,13 +3,14 @@ import { Camera, Canvas, Collider, GameObject, Transform, Object, Vector3 } from
 import { Button } from 'UnityEngine.UI';
 import UIManager, { UIPanel } from './Managers/UIManager';
 import { ZepetoPlayers } from 'ZEPETO.Character.Controller';
+import { WorldService } from 'ZEPETO.World';
 
 // This class is responsible for creating a floating icon upon which, when clicked, it will execute the game.
 export default class IconInteraction extends ZepetoScriptBehaviour {
 
     @SerializeField() private gameCanvas: GameObject; // Reference to the game canvas
 
-    @Header( "[Icon]" )
+    @Header("[Icon]")
     @SerializeField() private prefIconCanvas: GameObject; // Reference to the icon canvas
     @SerializeField() private iconPosition: Transform; // Reference to the transform that has the position where will be the icon
 
@@ -21,30 +22,26 @@ export default class IconInteraction extends ZepetoScriptBehaviour {
 
 
     // Update function called every frame
-    private Update () {
+    private Update() {
         // Check if the first trigger is done and the canvas is active
-        if ( this._isDoneFirstTrig && this._canvas?.gameObject.activeSelf )
-        {
+        if (this._isDoneFirstTrig && this._canvas?.gameObject.activeSelf) {
             this.UpdateIconRotation();
         }
     }
 
     // Function called when a collider enters the trigger
-    private OnTriggerEnter ( coll: Collider ) {
+    private OnTriggerEnter(coll: Collider) {
         // Check if the collider is not the local player's character collider
-        if ( coll != ZepetoPlayers.instance.LocalPlayer?.zepetoPlayer?.character.GetComponent<Collider>() )
-        {
+        if (coll.gameObject.name != WorldService.userId) {
             return;
         }
-
         this.ShowIcon();
     }
 
     // Function called when a collider exits the trigger
-    private OnTriggerExit ( coll: Collider ) {
+    private OnTriggerExit(coll: Collider) {
         // Check if the collider is not the local player's character collider
-        if ( coll != ZepetoPlayers.instance.LocalPlayer?.zepetoPlayer?.character.GetComponent<Collider>() )
-        {
+        if (coll != ZepetoPlayers.instance.LocalPlayer?.zepetoPlayer?.character.GetComponent<Collider>()) {
             return;
         }
 
@@ -52,37 +49,35 @@ export default class IconInteraction extends ZepetoScriptBehaviour {
     }
 
     // Show the icon
-    public ShowIcon () {
+    public ShowIcon() {
         // If it's the first trigger, create the icon; otherwise, show the existing canvas
-        if ( !this._isDoneFirstTrig )
-        {
+        if (!this._isDoneFirstTrig) {
             // Call the function to create the icon
             this.CreateIcon();
 
             // Set the flag on true
             this._isDoneFirstTrig = true;
-        } else
-        {
+        } else {
             // Active the canvas object
-            this._canvas.gameObject.SetActive( true );
+            this._canvas.gameObject.SetActive(true);
         }
         // Set the iconActive on true
         this._isIconActive = true;
     }
 
     // Hide the icon
-    public HideIcon () {
+    public HideIcon() {
         // If the canvas is created then deactivate the canvas
-        this._canvas?.gameObject.SetActive( false );
+        this._canvas?.gameObject.SetActive(false);
 
         // Set the iconActive on false
         this._isIconActive = false;
     }
 
     // Create the icon
-    private CreateIcon () {
+    private CreateIcon() {
         // Instantiate the prefIconCanvas as a new GameObject
-        const canvas = GameObject.Instantiate( this.prefIconCanvas, this.iconPosition ) as GameObject;
+        const canvas = GameObject.Instantiate(this.prefIconCanvas, this.iconPosition) as GameObject;
 
         // Get the Canvas component from the instantiated canvas
         this._canvas = canvas.GetComponent<Canvas>();
@@ -98,28 +93,28 @@ export default class IconInteraction extends ZepetoScriptBehaviour {
         this._canvas.worldCamera = this._cachedWorldCamera;
 
         // Add a listener to the button's onClick event
-        this._button.onClick.AddListener( () => {
+        this._button.onClick.AddListener(() => {
             this.OnClickIcon();
-        } );
+        });
     }
 
     // Update the icon rotation to face the camera
-    private UpdateIconRotation () {
-        this._canvas.transform.LookAt( this._cachedWorldCamera.transform, Vector3.down );
+    private UpdateIconRotation() {
+        this._canvas.transform.LookAt(this._cachedWorldCamera.transform, Vector3.down);
     }
 
     // Function called when the icon is clicked
-    private OnClickIcon () {
+    private OnClickIcon() {
         // Activate the game canvas, the start panel, the exit button and deactivate the game and end panels
-        this.gameCanvas.SetActive( true );
-        UIManager.instance.ShowPanel( UIPanel.Start );
-        UIManager.instance.exitBtn.gameObject.SetActive( true );
+        this.gameCanvas.SetActive(true);
+        UIManager.instance.ShowPanel(UIPanel.Start);
+        UIManager.instance.exitBtn.gameObject.SetActive(true);
 
         // Call to the function to hide de icon
         this.HideIcon();
 
         // Call to the function ControlPlayer
-        UIManager.instance.ControlPlayer( false );
+        UIManager.instance.ControlPlayer(false);
     }
 
 }
--- FindAPair_Template/Assets/PlayerSpawner.ts
+++ FindAPair_Template/Assets/PlayerSpawner.ts
@@ -1,25 +1,20 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script';
-import { LocalPlayer, SpawnInfo, UIZepetoPlayerControl, ZepetoCamera, ZepetoPlayers } from 'ZEPETO.Character.Controller';
+import { SpawnInfo, ZepetoPlayers } from 'ZEPETO.Character.Controller';
 import { WorldService } from 'ZEPETO.World';
-import { GameObject } from 'UnityEngine';
-import ThumbnailsCreator from './MainProject/_Scripts/ThumbnailsCreator';
 
 // This script spawns a single player
 export default class PlayerSpawner extends ZepetoScriptBehaviour {
 
-    // @SerializeField() thumbnailsCreatorObj: GameObject;
-    private thumbnailsCreator: ThumbnailsCreator;
-    Start () {
-        // this.thumbnailsCreator = this.thumbnailsCreatorObj.GetComponent<ThumbnailsCreator>();
+    Start() {
         // Grab the user id specified from logging into zepeto through the editor. 
-        ZepetoPlayers.instance.CreatePlayerWithUserId( WorldService.userId, new SpawnInfo(), true );
-        // ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener( () => {
-        //     const player: LocalPlayer = ZepetoPlayers.instance.LocalPlayer;
-        //     this.thumbnailsCreator.Setup( player.zepetoPlayer.userId );
+        ZepetoPlayers.instance.CreatePlayerWithUserId(WorldService.userId, new SpawnInfo(), true);
 
-        // } );
+        ZepetoPlayers.instance.OnAddedPlayer.AddListener((userId) => {
+            let player = ZepetoPlayers.instance.GetPlayer(userId);
+            player.character.gameObject.name = userId;
+        });
 
     }
 
-    
+
 }
